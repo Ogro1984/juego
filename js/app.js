5a85cef2d5d6ebca59b426dc84544b934f5edaa4
Ogro1984 <gonzalezracigmariano@gmail.com>
2019-02-11 22:55:04 -0300

    V2

--- js/app.js
+++ js/app.js
@@ -1,37 +1,49 @@
 
 var valordados=0;
 var a,b,c,d;
-var valores = [0];
-
+var flipflop=0;
+//funcion para generar numeros aleatorios del 1 al 4
 function dados(){
 valordados=Math.floor(Math.random() * 4) + 1;
 return valordados;
 }
 
-
-
-$("#putisio").click(function(){
-
-console.log("putimos");
-})
-
-
-$("#botonputo").click(function(){
-var a=dados();
-var b=dados();
-var c=dados();
-var d=dados();
-console.log(a,b,c,d);
-})
 //// cartel que cambia de color//
-var flipflop=0;
-
 window.onload = function() {
-setInterval(cambiarFondo, 500);// 5 segundos
+setInterval(cambiarFondo, 500);// 0.5 segundos
+llenadoincial();
 }
 
 function cambiarFondo() {
-  if (flipflop==0){$(".main-titulo").css("color","yellow");flipflop=1;}
+  if (flipflop==0){$(".main-titulo").css("color","#DCFF0E");flipflop=1;}
   else{$(".main-titulo").css("color","white");flipflop=0;}
 }
-//
+
+function llenadoincial(){
+
+a=1;
+$(".imgg").css("width","94px")
+}
+
+var myVar;
+function myFunction() {
+  myVar = setTimeout(alertFunc, 3000);
+}
+
+function alertFunc() {
+  alert("Hello!");}
+
+$(".btn-reinicio").click(function(){
+a=1;})
+
+/*
+$( "#up" ).click(function() {
+  $( "#img1" ).animate({ bottom: "-=50px" }, "slow" );
+});
+
+$( "#down" ).click(function(){
+  $( "#img1" ).animate({ bottom: "+=50px" }, "slow" );
+});
+*/
+$( ".sortable" ).sortable();
+$( ".sortable" ).disableSelection();
